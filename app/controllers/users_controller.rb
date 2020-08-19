2a2d3207ed00dd279655f944b32972cac80c55ac
vagrant <12ritsuya23@gmail.com>
2020-08-19 14:32:48 +0000

    Revert "[Update]users_controllerのshowアクション"
    This reverts commit eaaf01e9a3ff71859a593a8f89eed4506500fc9c.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,23 +4,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @current_user_entry = Entry.where(user_id: current_user.id)
-    @user_entry = entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
-      @current_user_entry.each do |current_user|
-        @user_entry.each do |user|
-          if current_user.room_id == user.room_id then
-            @isRoom = true
-            @roomId = current_user.room_id
-          end
-        end
-      end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end
     @work_outs = @user.work_outs.reverse_order
     @comment = Comment.new
   end

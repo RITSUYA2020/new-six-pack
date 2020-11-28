e96654d7c05c06a2efae8921f7280e92dec3f570
vagrant <12ritsuya23@gmail.com>
2020-11-28 10:18:29 +0000

    [Update]Todoコントローラーのcreateアクション、destroyアクションを記述

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,20 @@
 class TodosController < ApplicationController
     def create
+        todo = Todo.new(todo_params)
+        todo.user_id = current_user.id
+        todo.save
+        redirect_to request.referer
     end
 
     def destroy
+        todo = Todo.find(params[:id])
+        todo.destroy
+        redirect_to request.referer
+    end
+
+    private
+
+    def todo_params
+        params.require(:todo).permit(:task)
     end
 end
